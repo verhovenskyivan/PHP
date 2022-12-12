7bc85a0b3e4d2a8eebfa6e15da6b1ad9cdcd30b7
verhovensky <verhovenskyivan@gmail.com>
2022-12-12 18:23:55 +0300

    new oop practice + classes

---
+++ PHP/php.php
@@ -0,0 +1,23 @@
+<?php print ("Hello world");
+$name = "verhovensky";
+$welcome_text = "hello";
+$untited_string = "My favorite color is blue - ". $favorite_color . "and favorite number is - " . $favorite_number;
+print($untited_string);
+
+
+
+if ($gender == "Male")
+{
+    print "Hello, man";
+}
+else {
+    print "hello, +";
+}
+
+
+$fav_shows = ["game of thrones"[0], "cyberpunk edgerunners"[1]];
+
+print("Мои любимые сериалы: " . $fav_shows);
+?>
+
+\
